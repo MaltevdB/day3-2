1be3ac67181a06cf82ba29850aa92a6205bd8404
MaltevdB <91481810+MaltevdB@users.noreply.github.com>
2021-10-07 12:22:51 +0200

    Update example.py

--- example.py
+++ example.py
@@ -10,6 +10,16 @@ def test_add():
     assert add(0.1, 0.2) == approx(0.3)
 
 
+def subtract(a, b):
+    return a + b  # <--- fix this 
+
+
+# uncomment the following test
+#def test_subtract():
+#    assert subtract(2, 3) == -1
+
+
+
 def print_temperature():
 	temperature = 10
 	print(temperature)

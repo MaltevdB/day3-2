3130fe15b39d73754a2b943d4a4796ecc223f693
MaltevdB <91481810+MaltevdB@users.noreply.github.com>
2021-10-07 12:14:00 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,15 @@
+from pytest import approx
+
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+    assert add(0.1, 0.2) == approx(0.3)
+
+
 def print_temperature():
 	temperature = 10
 	print(temperature)
